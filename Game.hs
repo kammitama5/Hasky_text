94d6fc8346c83c38c9f0a57592da64c6d429f0f3
kammitama5 <krystal.maughan@gmail.com>
2018-04-11 14:38:19 -0700

    draft for simple text game to work on Thurs&Fri

---
+++ Game.hs
@@ -0,0 +1,36 @@
+module Game where
+
+-- welcome game
+-- Hello brave one, what is your name?
+
+-- would you like to play? 
+-- Press Y for yes or Q to quit
+
+-- you are in a dark cave
+-- what would you like to do?
+-- (1) stay there
+    -- if you do, a demon monster awakes from a slumber and kills you. The end.
+-- (2) find an entrance
+    -- you awaken to find yourself in a thick forest. You find an elfin friend playing bagpipes.
+    -- He offers to give you a key in exchange for your firstborn child. What would you like to do?
+        -- (1) Heck no. I'm not giving my firstborn 
+            -- You descend into insanity. The elf is evil, butchers you and feeds you to the fishes. 
+            -- Game over.
+        -- (2) Meh. I'm okay. Give him the kid.
+            -- Congrats! You have beaten the game! Victory is yours forever!
+            -- You won the game but will live in misery forever that you gave your firstborn away.
+            -- Terrible parent!
+
+
+
+-- ---------------------------
+
+-- main loop
+
+-- start game
+
+
+
+-- quit game
+
+-- ----------------------------
